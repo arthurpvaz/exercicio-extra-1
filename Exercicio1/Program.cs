648fc0a62a7c07d11a50d8f2839300e6a6944cfb
Arthur Vaz <pv.arthur10@gmail.com>
2025-01-31 12:18:58 -0300

    Primeira refatoracao para economia de codigo

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -17,51 +17,43 @@ namespace Exercicio1
                 Console.Clear();
                 showMenu();
 
-                try
+
+                int option = ReadPositiveInt("Digite sua opção: ");
+
+                switch (option)
                 {
-                    Console.Write("Digite sua opção: ");
-                    int option = int.Parse(Console.ReadLine());
+                    case 1:
+                        callAddProduct();
+                        Console.ReadKey();
+                        break;
 
-                    switch (option)
-                    {
-                        case 1:
-                            callAddProduct();
-                            Console.ReadKey();
-                            break;
+                    case 2:
+                        callRemoveProduct();
+                        Console.ReadKey();
+                        break;
 
-                        case 2:
-                            callRemoveProduct();
-                            Console.ReadKey();
-                            break;
+                    case 3:
+                        callUpdateStock();
+                        Console.ReadKey();
+                        break;
 
-                        case 3:
-                            callUpdateStock();
-                            Console.ReadKey();
-                            break;
+                    case 4:
+                        callListProduct();
+                        Console.ReadKey();
+                        break;
 
-                        case 4:
-                            callListProduct();
-                            Console.ReadKey();
-                            break;
+                    case 5:
+                        running = false;
+                        Console.WriteLine("Programa finalizado!");
+                        break;
 
-                        case 5:
-                            running = false;
-                            Console.WriteLine("Programa finalizado!");
-                            break;
-
-                        default:
-                            Console.WriteLine("Opção digitada inválida!");
-                            Console.ReadKey();
-                            break;
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("O valor digitado deve ser um número inteiro!");
-                    Console.ReadKey();
+                    default:
+                        Console.WriteLine("Opção digitada inválida!");
+                        Console.ReadKey();
+                        break;
                 }
             }
-        }    
+        }
 
         static void showMenu()
         {
@@ -85,111 +77,14 @@ namespace Exercicio1
 
         static void callAddProduct()
         {
-            int productCode = 000;
-            string name = "";
-            decimal price = 0;
-            int stock = 0;
-            Category category = 0;
-
             Console.WriteLine("Adicionando produto...");
             Console.WriteLine("Digite as informações do produto abaixo");
 
-            bool codeIsValid = false;
-            bool nameIsValid = false;
-            bool priceIsValid = false;
-            bool stockIsValid = false;
-            bool categoryIsValid = false;
-
-            do 
-            {
-                try
-                {
-                    Console.Write("Código: ");
-                    productCode = int.Parse(Console.ReadLine());
-                    codeIsValid = true;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("O código deve ser digitado somente com números inteiros! Tente novamente.");
-                }
-            } while (!codeIsValid);
-
-            do
-            {
-                Console.Write("Nome: ");
-                name = Console.ReadLine();
-                nameIsValid = true;
-
-                if (name.Length < 2)
-                {
-                    Console.WriteLine("Nome deve conter 2 (dois) ou mais caracteres! Tente novamente.");
-                    nameIsValid = false;
-                }
-            } while (!nameIsValid);
-
-            do
-            {
-                try
-                {
-                    Console.Write("Preço: R$");
-                    price = decimal.Parse(Console.ReadLine());
-                    priceIsValid = true;
-
-                    if (price <= 0)
-                    {
-                        Console.WriteLine("Inválido! Valor não pode ser menor ou igual a zero! Tente novamente.");
-                        priceIsValid = false;
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("O preço deve ser digitado com número real! Tente novamente.");
-                }
-            } while (!priceIsValid);
-
-            do
-            {
-                try
-                {
-                    Console.Write("Quantidade em estoque: ");
-                    stock = int.Parse(Console.ReadLine());
-                    stockIsValid = true;
-
-                    if (stock <= 0)
-                    {
-                        Console.WriteLine("Quantidade em estoque não pode ser menor que 1 (um)! Tente novamente.");
-                        stockIsValid = false;
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("A quantidade em estoque deve ser digitada com um número inteiro! Tente novamente.");
-                }
-            } while (!stockIsValid);
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("Escolha a categoria:");
-                    Console.WriteLine("1 - Alimentos");
-                    Console.WriteLine("2 - Eletrônicos");
-                    Console.WriteLine("3 - Vestuário");
-                    Console.Write("Opção: ");
-                    category = (Category)int.Parse(Console.ReadLine());
-                    categoryIsValid = true;
-
-                    if (category <= 0 || category > Category.Vestuario)
-                    {
-                        Console.WriteLine("Opção inválida! Tente novamente.");
-                        categoryIsValid = false;
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Valor digitado incorreto! Tente novamente.");
-                }
-            } while (!categoryIsValid);
+            int productCode = ReadPositiveInt("Código: ");
+            string name = ReadString("Nome: ");
+            decimal price = ReadDecimal("Preço: ");
+            int stock = ReadPositiveInt("Quantidade em estoque: ");
+            Category category = ReadCategory();
 
             Product product = new Product(productCode, name, price, stock, category);
 
@@ -198,8 +93,7 @@ namespace Exercicio1
 
         static void callRemoveProduct()
         {
-            Console.Write("Insira o código do produto que deseja remover: ");
-            int productCode = int.Parse(Console.ReadLine());
+            int productCode = ReadPositiveInt("Insira o código do produto que deseja remover: ");
 
             Console.WriteLine("Removendo produto...");
 
@@ -208,11 +102,9 @@ namespace Exercicio1
 
         static void callUpdateStock()
         {
-            Console.Write("Digite o código do produto que deseja atualizar: ");
-            int productCode = int.Parse(Console.ReadLine());
+            int productCode = ReadPositiveInt("Digite o código do produto que deseja atualizar: ");
 
-            Console.Write("Insira a quantidade para qual deseja atualizar: ");
-            int newStock = int.Parse(Console.ReadLine());
+            int newStock = ReadInt("Insira a quantidade para qual deseja atualizar: ");
 
             productService.UpdateStock(productCode, newStock);
 
@@ -223,6 +115,93 @@ namespace Exercicio1
         {
             Console.WriteLine("Listagem dos produtos cadastrados...");
             productService.ListProducts();
+        }
+
+        static int ReadInt(string message)
+        {
+            int value;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Entrada inválida! Digite um número inteiro.");
+            }
+        }
+
+        static int ReadPositiveInt(string message)
+        {
+            int value;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                if (int.TryParse(Console.ReadLine(), out value) && value > 0)
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Entrada inválida! Digite um número inteiro positivo.");
+            }
+        }
+
+        static decimal ReadDecimal(string message)
+        {
+            decimal value;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                if (decimal.TryParse(Console.ReadLine(), out value) && value > 0)
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Entrada inválida! Digite um número decimal maior que zero.");
+            }
+        }
+
+        static string ReadString(string message, int minLength = 2)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string input = Console.ReadLine().Trim();
+
+                if (input.Length >= minLength)
+                {
+                    return input;
+                }
+
+                Console.WriteLine($"Entrada inválida! O valor deve ter pelo menos {minLength} caracteres.");
+            }
+        }
+
+        static Category ReadCategory()
+        {
+            while (true)
+            {
+                Console.WriteLine("Escolha a categoria:");
+                Console.WriteLine("1 - Alimentos");
+                Console.WriteLine("2 - Eletrônicos");
+                Console.WriteLine("3 - Vestuário");
+                Console.Write("Opção: ");
+                Category value;
+
+                if (Category.TryParse(Console.ReadLine(), out value) && value > 0 && value <= Category.Vestuario)
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Entrada inválida! Digite um número correspondente a uma categoria.");
+            }
         }
     }
 }
